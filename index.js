a00db102cef0d179397cc4c0b6cd2df80f8bcbd9
MHaurel <maxime.haurel0@gmail.com>
2022-03-02 14:40:58 +0100

    Devloping website from sketch figma
    - Builiding all areas (education, projects, ...)
    - Updating sticky navbar with color when user on divs (need to fix margin alignment and colors of navbar)

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {updateOnScroll()};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
+var profileDiv = document.getElementById("index");
+var educationDiv = document.getElementById("education");
+var projectsDiv = document.getElementById("projects");
+var contactDiv = document.getElementById("contact");
+
+updateOnScroll();
+
+function updateOnScroll() {
+  var y = window.pageYOffset;
+  if (y >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
+  }
+
+  if (y >= profileDiv.offsetTop && y < educationDiv.offsetTop) {
+    clearHighlightNavbar();
+    // Highlight profile navbar button
+    navbar.children[0].classList.add("highlighted");
+  } 
+
+  if (y >= educationDiv.offsetTop && y < projectsDiv.offsetTop) {
+    clearHighlightNavbar();
+    // Highlight education navbar button
+    navbar.children[1].classList.add("highlighted");
+  } 
+
+  if (y >= projectsDiv.offsetTop && y < contactDiv.offsetTop) {
+    clearHighlightNavbar();
+    // Highlight projects navbar button
+    navbar.children[2].classList.add("highlighted");
+  } 
+
+  if (y >= contactDiv.offsetTop) {
+    clearHighlightNavbar();
+    // Highlight contact navbar button
+    navbar.children[3].classList.add("highlighted");
+  } 
+}
+
+function clearHighlightNavbar() {
+  for (var i = 0; i < navbar.children.length; i++) {
+    navbar.children[i].classList.remove("highlighted");
   }
 }
